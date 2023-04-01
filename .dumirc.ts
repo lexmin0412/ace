7d932e179fd0eb27a7c12d630a0c65a12b56879a
lexmin0412 <zhangle_dev@outlook.com>
2023-04-02 02:23:00 +0800

    chore: add publicPath for github pages deploy

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   base: '/antd-components-enhanced',
+  publicPath: '/antd-components-enhanced/',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'AntD CE',
