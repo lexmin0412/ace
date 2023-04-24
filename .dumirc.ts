c5073f954e8a586a2cd0abc11add76dcc7c0fa58
lexmin0412 <zhangle_dev@outlook.com>
2023-04-24 22:38:20 +0800

    chore: custom nav links

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   publicPath: '/antd-components-enhanced/',
   outputPath: 'docs-dist',
   themeConfig: {
+    nav: [
+      { title: 'Guide', link: '/guide' },
+      { title: 'Components', link: '/components/color-picker' },
+    ],
     name: 'AntD CE',
     footer: `<div>Copyright © 2023 | Powered by <a href="https://d.umijs.org/guide" target="_blank">dumi</a> | Authored by <a href="https://github.com/lexmin0412" target="_blank">Lexmin0412</a>.</div>`,
     socialLinks: {
