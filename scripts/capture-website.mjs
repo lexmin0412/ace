4f97976cadf9246ee54ed9a694e5467cd4a045e2
lexmin0412 <zhangle_dev@outlook.com>
2023-04-24 23:18:26 +0800

    chore: generate docs index page screenshots in ci

---
+++ scripts/capture-website.mjs
@@ -0,0 +1,15 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+import captureWebsite from 'capture-website';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const screenshotPath = path.resolve(__dirname, '..', 'docs_index.png')
+
+console.log('fs.existsSync(screenshotPath)', fs.existsSync(screenshotPath))
+if (fs.existsSync(screenshotPath)) {
+  fs.rmSync(screenshotPath)
+}
+
+await captureWebsite.file('https://lexmin0412.github.io/antd-components-enhanced', 'docs_index.png');
